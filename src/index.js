6d912eec3e11c9ac0731ccb362bf5b226b748669
Greggy Gianini Firmansyah <greggygf@gmail.com>
2020-03-29 12:53:11 +0700

    praktikum js 5 react router

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import ReactDOM from 'react-dom';
 // Mengimport Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-// import App from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 // import HelloComponent from './component/HelloComponent';
 // import LoginComponent from './component/LoginComponent';
 // import StatefullComponent from './container/StatefullComponent';
 
 // import BlogPost from './container/BlogPost/BlogPost';
-import Mahasiswa from './container/Mahasiswa/Mahasiswa';
+// import Mahasiswa from './container/Mahasiswa/Mahasiswa';
 
 // function HelloWord()
 // {
@@ -30,7 +30,7 @@ import Mahasiswa from './container/Mahasiswa/Mahasiswa';
 //     }
 // }
 
-ReactDOM.render( < Mahasiswa / > , document.getElementById('content'));
+ReactDOM.render( < App / > , document.getElementById('content'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
